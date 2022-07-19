1b8c3abf68727c30fc967baaa1e8be101e357359
Nguyen Chi Viet <nguyenchiviet.cv@gmail.com>
2022-07-19 12:43:44 +0700

    upload source code

--- slideshow/script.js
+++ slideshow/script.js
@@ -9,60 +9,54 @@ const pagination = document.querySelector('.pagination')
 const paginationBtns = document.querySelectorAll('.paginate-btn')
 // Else
 let counter = 0;
-let currentIndex = 0;
-let size = slidesImages[counter||currentIndex].clientWidth;
+let size = slidesImages[counter].clientWidth;
 
-// Next/Prev slides onclick (Manual)
+// Next Button click
 function next() {
-    var currentActive = paginationBtns[counter];
-    counter++;
-    if (counter > slidesImages.length - 1) {
-        counter = 0;
+    // 1. Add class active
+    // 2. Chuyển slide
+    var currentBtn = paginationBtns[counter]
+
+    ++counter
+    if (counter > paginationBtns.length - 1) {
+        counter = 0
     }
-     
-    currentActive.classList.remove('active'); //Remove Class active
-    paginationBtns[counter].classList.add('active'); //Add class active
-    slides.style.transform = 'translateX(-' +(size * counter) + 'px)'; //Transform slide
-    currentIndex = counter; //update current slide
+
+    paginationBtns[counter].classList.add('active')
+    currentBtn.classList.remove('active')
+    slides.style.transform = 'translate(-' + (size * counter) + 'px)'
 }
-    
-// Prev slides onclick (Manual)
+
+// Prev button click
 function prev() {
-    var currentActive = paginationBtns[counter];
-    counter--;
+    // 1. Add class active
+    // 2. Chuyển slide
+    var currentBtn = paginationBtns[counter]
+
+    --counter
     if (counter < 0) {
-        counter = slidesImages.length - 1
+        counter = paginationBtns.length - 1
     }
 
-    currentActive.classList.remove('active'); //Remove Class active
-    paginationBtns[counter].classList.add('active'); //Add class active
-    slides.style.transform = 'translateX(-' +(size * counter) + 'px)'; //Transform slide
-    currentIndex = counter; //update current slide
+    paginationBtns[counter].classList.add('active')
+    currentBtn.classList.remove('active')
+    slides.style.transform = 'translate(-' + (size * counter) + 'px)'
 }
 
-// Pagination onclick (Manual)
+// Pagination click (Manual)
 function paginationClick() {
     for (let i = 0; i < paginationBtns.length; i++) {
         paginationBtns[i].addEventListener('click', () => {
-
-            paginationBtns[currentIndex].classList.remove('active'); //Remove Class active
-            paginationBtns[i].classList.add('active'); //Add class active
-            slides.style.transform = 'translateX(-' +(size * i) + 'px)'; //Transform slide
-            currentIndex = i; //update current slide
-        })  
+            paginationBtns[i].classList.add('active')
+            paginationBtns[counter].classList.remove('active')
+            counter = i
+            slides.style.transform = 'translate(-' + (size * counter) + 'px)'
+        })
     }
 }
 paginationClick()
 
 // Auto next slide
 setInterval(() => {
-    const currentSlide = paginationBtns[currentIndex];
-    currentIndex++;
-    if (currentIndex > slidesImages.length - 1) {
-        currentIndex = 0;
-    }
-    
-    currentSlide.classList.remove('active'); //Remove Class active
-    paginationBtns[currentIndex].classList.add('active'); //Add class active
-    slides.style.transform = 'translateX(-' +(size * currentIndex) + 'px)'; //Transform slide
+    next()
 }, 5000)
